f06973c3157fe65d156b512b5e994ac836a7741d
okistuff <probro1802@gmail.com>
2020-07-03 20:29:52 -0400

    💻 Version 1.0.0 Added

--- lib/shapesrb.rb
+++ lib/shapesrb.rb
@@ -1,1 +1,27 @@
-puts "Hello World"
+
+class Shapes
+    def self.TRIANGLE
+        puts "   /|"
+        puts "  / |"
+        puts " /  |"
+        puts "/___|"
+    end
+
+    def self.SQUARE
+        puts " __________"
+        puts "|          |"
+        puts "|          |"
+        puts "|          |"
+        puts "|__________|"
+    end
+    
+    def self.RECTANGLE
+        puts " _________________"
+        puts "|                 |"
+        puts "|                 |"
+        puts "|                 |"
+        puts "|_________________|"
+
+    end
+
+end
